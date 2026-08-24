480533ecaf908e580cb5ea0e65bf292be7b09879
Martin <martinva2006arm@gmail.com>
2026-08-24 17:51:52 -0400

    Avance en base de datos y pruebas

--- database.py
+++ database.py
@@ -2,9 +2,18 @@ import sqlite3
 
 def conectar():
     conexion = sqlite3.connect("database/business.db")
+    conexion.execute("PRAGMA foreign_keys = ON")
     return conexion
 
 def crear_tablas():
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+def crear_categorias(nombre, descripcion):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+def crear_productos(nombre, descripcion, precio, stock, disponible, categoria_id):
     conexion = conectar()
     cursor = conexion.cursor()
 
@@ -13,6 +22,19 @@ def crear_tablas():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT NOT NULL,
             descripcion TEXT NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS productos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            descripcion TEXT NOT NULL,
+            precio INTEGER NOT NULL,
+            stock INTEGER NOT NULL DEFAULT 0,
+            disponible BOOLEAN NOT NULL DEFAULT 1,
+            categoria_id INTEGER NOT NULL,
+            FOREIGN KEY (categoria_id) REFERENCES categorias(id)
         )
     ''')
 
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from database import conectar, crear_tablas
+from database import conectar, crear_tablas, crear_categorias
 
 crear_tablas()
+
+crear_categorias("Electrónica", "Dispositivos electrónicos y gadgets")
+
+print("Tablas creadas y categoría insertada correctamente.")
 
 conexion = conectar()
 
 print("Conexión a la base de datos establecida correctamente.")
 
-conexion.close()
+conexion.close()    
