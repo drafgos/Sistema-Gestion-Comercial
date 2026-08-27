bdee5240cc84b3aeff98a56a667feb6d8d9d8674
Martin <martinva2006arm@gmail.com>
2026-08-27 12:24:28 -0400

    Agregar funciones nuevas, tablas de pedidos y detalles de pedidos y arreglar errores no corregidos

--- database.py
+++ database.py
@@ -9,14 +9,6 @@ def crear_tablas():
     conexion = conectar()
     cursor = conexion.cursor()
 
-def crear_categorias(nombre, descripcion):
-    conexion = conectar()
-    cursor = conexion.cursor()
-
-def crear_productos(nombre, descripcion, precio, stock, disponible, categoria_id):
-    conexion = conectar()
-    cursor = conexion.cursor()
-
     cursor.execute(''' 
         CREATE TABLE IF NOT EXISTS categorias (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,6 +29,83 @@ def crear_productos(nombre, descripcion, precio, stock, disponible, categoria_id
             FOREIGN KEY (categoria_id) REFERENCES categorias(id)
         )
     ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS pedidos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cliente_nombre TEXT NOT NULL,
+            estado TEXT NOT NULL DEFAULT 'tomado',
+            fecha_creacion TEXT NOT NULL DEFAULT (datetime('now')),
+            CHECK (estado IN ('tomado','preparando', 'enviado', 'entregado'))
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS detalle_pedido (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            pedido_id INTEGER NOT NULL,
+            producto_id INTEGER NOT NULL,
+            cantidad INTEGER NOT NULL,
+            precio_unitario INTEGER NOT NULL,
+            FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+            FOREIGN KEY (producto_id) REFERENCES productos(id)
+        )
+    ''')
+
+    conexion.commit()
+    conexion.close()
+
+def crear_categorias(nombre, descripcion):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute('''
+        INSERT INTO categorias (nombre, descripcion) VALUES (?, ?)
+    ''', (nombre, descripcion))
+
+    conexion.commit()
+    conexion.close()
+
+def crear_productos(nombre, descripcion, precio, stock, disponible, categoria_id):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute('''
+        INSERT INTO productos (nombre, descripcion, precio, stock, disponible, categoria_id) VALUES (?, ?, ?, ?, ?, ?)
+    ''', (nombre, descripcion, precio, stock, disponible, categoria_id))
+
+    conexion.commit()
+    conexion.close()
+
+def crear_pedidos(cliente_nombre):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute('''
+        INSERT INTO pedidos (cliente_nombre) VALUES (?)
+    ''', (cliente_nombre,))
+
+    conexion.commit()
+    conexion.close()
+
+def agregar_detalle_pedido(pedido_id, producto_id, cantidad, precio_unitario):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute('''
+        INSERT INTO detalle_pedido (pedido_id, producto_id, cantidad, precio_unitario) VALUES (?, ?, ?, ?)
+    ''', (pedido_id, producto_id, cantidad, precio_unitario))
+
+    conexion.commit()
+    conexion.close()
+
+def actualizar_estado_pedido(pedido_id, nuevo_estado):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute('''
+        UPDATE pedidos SET estado = ? WHERE id = ?
+    ''', (nuevo_estado, pedido_id))
 
     conexion.commit()
     conexion.close()
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from database import conectar, crear_tablas, crear_categorias
+from database import conectar, crear_tablas, crear_categorias, crear_productos
 
 crear_tablas()
 
 crear_categorias("Electrónica", "Dispositivos electrónicos y gadgets")
 
 print("Tablas creadas y categoría insertada correctamente.")
+
+crear_productos("iPhone 13", "Smartphone de última generación", 650.990, 10, 1, 1)
+
+print("Producto insertado correctamente.")
 
 conexion = conectar()
 
